1571aa778842a507664f3b97b601ed1a68a0819e
SHYAM SUNDAR R <rshyamsundar.in@gmail.com>
2025-10-05 14:04:59 +0530

    Update First-Non-Repeating-Character.java

--- First-Non-Repeating-Character.java
+++ First-Non-Repeating-Character.java
@@ -24,3 +24,23 @@ class Main {
         }
     }
 }
+
+
+// Index of the First Non-Repeating Character in a String
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;        
+    }
+}
